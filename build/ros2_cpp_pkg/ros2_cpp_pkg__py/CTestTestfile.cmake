49f46220e1c29362967c461e2f177fd2ad91670f
Paulo Padrao <55985904+pvpadrao@users.noreply.github.com>
2023-02-16 14:13:58 -0500

    Delete build directory

--- build/ros2_cpp_pkg/ros2_cpp_pkg__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /lclhome/plope113/ros2_cpp_ws/build/ros2_cpp_pkg/ros2_cpp_pkg__py
-# Build directory: /lclhome/plope113/ros2_cpp_ws/build/ros2_cpp_pkg/ros2_cpp_pkg__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
