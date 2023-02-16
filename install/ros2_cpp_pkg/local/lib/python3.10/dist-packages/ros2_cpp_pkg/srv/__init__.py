cdf86a4d5249d541505f1bcfff4c968e39ef3e09
Paulo Padrao <55985904+pvpadrao@users.noreply.github.com>
2023-02-16 14:14:10 -0500

    Delete install directory

--- install/ros2_cpp_pkg/local/lib/python3.10/dist-packages/ros2_cpp_pkg/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from ros2_cpp_pkg.srv._odd_even_check import OddEvenCheck  # noqa: F401
--- install/ros2_cpp_pkg/share/ros2_cpp_pkg/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ros2_cpp_pkg_IDL_FILES "srv/OddEvenCheck.idl")
-set(ros2_cpp_pkg_INTERFACE_FILES "srv/OddEvenCheck.srv;srv/OddEvenCheck_Request.msg;srv/OddEvenCheck_Response.msg")
